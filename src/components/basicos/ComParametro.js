0dad494c6f4f588712f05b1a3f4fdf6935599705
GugaSantos <guga.santos.guga@gmail.com>
2023-02-28 15:49:34 -0300

    fundamentos 01 e 02

---
+++ src/components/basicos/ComParametro.js
@@ -0,0 +1,8 @@
+export default function ComParametro(props) {
+  return (
+    <div>
+      <h2>{ props.titulo }</h2>
+      <h3>{ props.subtitulo }</h3>
+    </div>
+  )
+}
---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,8 @@
+export default function Primeiro() {
+  return (
+    <div>
+      <h2>Primeiro Componente</h2>
+      <p>Seja bem-vindo!</p>
+    </div>  
+  )
+}
